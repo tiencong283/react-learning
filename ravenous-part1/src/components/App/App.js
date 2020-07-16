8b15b05f6bcd108964759d4b5fcbf5794499634e
tiencong283 <tiencong283@outlook.com>
2020-07-17 00:01:56 +0700

    add authorization-form,ravenous-part1 projects

---
+++ ravenous-part1/src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./App.css"
+import SearchBar from "../SearchBar/SearchBar"
+import BusinessList from "../BusinessList/BusinessList"
+
+function App() {
+  return (
+    <div class="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  )
+}
+
+export default App
